bd860c1927e8abf426dae7f939d3759e2b215b68
Dedyanna Cacau <dedyannacacau@gmail.com>
2020-10-19 16:06:04 -0300

    atualizações

---
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import { Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import { Link } from 'react-router-dom';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
+
+import mapMarkerImg from '../../assets/images/map-marker.svg';
+import Map from '../../components/Map';
+
+import './styles.css';
+import api from '../../services/api';
+
+interface Orphanage {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+}
+
+const happyMapIcon = L.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+export default function OrphanagesMap() {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  useEffect(() => {
+    api.get('orphanages').then((response) => {
+      setOrphanages(response.data);
+    });
+  }, []);
+
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Ceará</strong>
+          <span>Fortaleza</span>
+        </footer>
+      </aside>
+
+      <Map>
+        {orphanages.map((orphanage) => {
+          return (
+            <Marker
+              icon={happyMapIcon}
+              position={[orphanage.latitude, orphanage.longitude]}
+              key={orphanage.id}
+            >
+              <Popup
+                closeButton={false}
+                minWidth={240}
+                maxWidth={240}
+                className="map-popup"
+              >
+                {orphanage.name}
+                <Link to={`/orphanages/${orphanage.id}`}>
+                  <FiArrowRight size={20} color="#fff" />
+                </Link>
+              </Popup>
+            </Marker>
+          );
+        })}
+      </Map>
+
+      <Link to="/orphanages/create" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
+}
+
+// return `https://a.tile.openstreetmap.org/${z}/${x}/${y}.png`;
